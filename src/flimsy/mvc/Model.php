68ca630fa643e692c4f788db3edcb00979bc24d2
Marvin Blum <marvin@marvinblum.de>
2015-06-30 19:55:47 +0200

    Added static controller for basic needs.

--- src/flimsy/mvc/Model.php
+++ src/flimsy/mvc/Model.php
@@ -1,5 +1,5 @@
 <?php
-class Model{
+interface Model{
 	
 }
 ?>
---
+++ src/flimsy/mvc/StaticController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Basic controller that just displays a view.
+ */
+class StaticController extends Controller{
+	function __construct($view){
+		Controller::__construct($view);
+	}
+
+	function exec(array $get){
+		$this->view->display();
+	}
+}
+?>
